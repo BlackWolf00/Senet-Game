cbdffbeeb879e5a43286a2ef05d3c226e2bb69dd
Matteo Tossuto <matteotossuto@gmail.com>
2025-03-27 01:29:59 +0100

    Refactor + add move pieces

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,34 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SenetGame());
+  runApp(SenetApp());
 }
 
-class SenetGame extends StatefulWidget {
+class SenetApp extends StatefulWidget {
   @override
-  _SenetGameState createState() => _SenetGameState();
+  _SenetAppState createState() => _SenetAppState();
 }
 
-class _SenetGameState extends State<SenetGame> {
-  Map<int, String> board = {
-    0: 'cone', 1: 'spool', 2: 'cone', 3: 'spool', 4: 'cone',
-    5: 'spool', 6: 'cone', 7: 'spool', 8: 'cone', 9: 'spool',
-  };
+class _SenetAppState extends State<SenetApp> {
+  List<int?> board = List.filled(30, null);
+  int currentPlayer = 1;
+  int? selectedPiece;
+  int? diceRoll;
 
-  Map<int, String> specialTiles = {
-    14: 'house_of_life',
-    25: 'house_of_happiness',
-    26: 'house_of_water',
-    27: 'exit',
-    28: 'exit',
-    29: 'exit',
-  };
+  @override
+  void initState() {
+    super.initState();
+    initializeBoard();
+  }
 
-  int diceRoll = 0;
+  void initializeBoard() {
+    // Posiziona le pedine nelle prime 10 caselle, alternate tra i giocatori
+    for (int i = 0; i < 10; i++) {
+      board[i] = (i % 2 == 0) ? 1 : 2;
+    }
+    setState(() {});
+  }
 
   void rollDice() {
     int count = 0;
@@ -38,65 +41,82 @@ class _SenetGameState extends State<SenetGame> {
     });
   }
 
+  void selectPiece(int index) {
+    if (board[index] == currentPlayer) {
+      setState(() {
+        selectedPiece = index;
+      });
+    }
+  }
+
+  void movePiece() {
+    if (selectedPiece != null && diceRoll != null) {
+      int newPosition = selectedPiece! + diceRoll!;
+      if (newPosition < 30 && board[newPosition] == null) {
+        setState(() {
+          board[newPosition] = currentPlayer;
+          board[selectedPiece!] = null;
+          selectedPiece = null;
+          diceRoll = null;
+          currentPlayer = (currentPlayer == 1) ? 2 : 1;
+        });
+      }
+    }
+  }
+
+  Color getTileColor(int index) {
+    if (index == 14) return Colors.green; // Casa della Vita
+    if (index == 25) return Colors.yellow; // Casa della Felicità
+    if (index == 26) return Colors.blue; // Casa dell'Acqua
+    if (index >= 27) return Colors.orange; // Uscita
+    return Colors.brown.shade300; // Normale
+  }
+
+  Widget getPiece(int? player) {
+    if (player == 1) {
+      return Icon(Icons.circle, color: Colors.red, size: 24);
+    } else if (player == 2) {
+      return Icon(Icons.square, color: Colors.blue, size: 24);
+    } else {
+      return SizedBox.shrink();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Senet Game')),
+        appBar: AppBar(title: Text('Senet')),
         body: Column(
           children: [
-            SizedBox(height: 20),
-            Text(
-              'Lancio: $diceRoll',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 10,
+              ),
+              itemCount: 30,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () => selectPiece(index),
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: getTileColor(index),
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: Center(child: getPiece(board[index])),
+                  ),
+                );
+              },
             ),
             ElevatedButton(
               onPressed: rollDice,
-              child: Text('Lancia i bastoncini!'),
+              child: Text('Lancia i bastoncini'),
             ),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 10,
-                ),
-                itemCount: 30,
-                itemBuilder: (context, index) {
-                  Color tileColor = Colors.brown[300]!;
-
-                  if (specialTiles.containsKey(index)) {
-                    switch (specialTiles[index]) {
-                      case 'house_of_life':
-                        tileColor = Colors.green;
-                        break;
-                      case 'house_of_happiness':
-                        tileColor = Colors.blue;
-                        break;
-                      case 'house_of_water':
-                        tileColor = Colors.lightBlue;
-                        break;
-                      case 'exit':
-                        tileColor = Colors.yellow;
-                        break;
-                    }
-                  }
-
-                  return Container(
-                    margin: EdgeInsets.all(2),
-                    decoration: BoxDecoration(
-                      color: tileColor,
-                      border: Border.all(color: Colors.black, width: 1),
-                    ),
-                    child: Center(
-                      child: board.containsKey(index)
-                          ? Text(
-                        board[index] == 'cone' ? '🔺' : '⚫',
-                        style: TextStyle(fontSize: 24),
-                      )
-                          : null,
-                    ),
-                  );
-                },
-              ),
+            if (diceRoll != null) Text('Risultato: $diceRoll'),
+            ElevatedButton(
+              onPressed: movePiece,
+              child: Text('Muovi pezzo'),
             ),
           ],
         ),
